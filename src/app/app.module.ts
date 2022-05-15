da0e029aa1d3ba064cafc268e1e08941d4ef0be7
Vaibhav Gaur <vaibhavgaur56789@gmail.com>
2022-05-15 14:00:18 +0530

    Route-Guard service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { LogoutComponent } from './logout/logout.component';
 
 //Every Angular component (@Component) has to be associated with an Angular Module (@NgModule).
 
@@ -21,7 +22,8 @@ import { FooterComponent } from './footer/footer.component';
     ErrorComponent,
     ListTodosComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    LogoutComponent
   ], 
   imports: [
     BrowserModule,
--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -8,10 +8,10 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password) {
-    console.log('before ' + this.isUserLoggedIn());
+    //console.log('before ' + this.isUserLoggedIn());
     if(username === "Vaibhav" && password === 'dummy') {
       sessionStorage.setItem('authenticateUser', username);
-      console.log('after ' + this.isUserLoggedIn());
+      //console.log('after ' + this.isUserLoggedIn());
       return true;
     }
     else {
@@ -22,5 +22,9 @@ export class HardcodedAuthenticationService {
   isUserLoggedIn() {
     let user = sessionStorage.getItem('authenticateUser')
     return !(user === null)
+  }
+
+  logout() {
+    sessionStorage.removeItem('authenticateUser')
   }
 }
