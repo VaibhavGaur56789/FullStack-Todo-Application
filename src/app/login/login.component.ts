89e0e34991cc8ff37cba866fc5d643be34f54cd5
Vaibhav Gaur <vaibhavgaur56789@gmail.com>
2022-06-12 10:10:34 +0530

    Connecting JWT Framework with Spring Security and Spring boot

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -56,4 +56,19 @@ export class LoginComponent implements OnInit {
         )  
   }
 
+  handleJWTAuthLogin() { //Event Binding
+    
+    this.basicAuthenticationService.executeJWTAuthenticationService(this.username, this.password)
+        .subscribe(
+          data => {
+            console.log(data)
+            this.router.navigate(['welcome', this.username])
+            this.invalidLogin = false
+          },
+          error => {
+            console.log(error)
+            this.invalidLogin = true 
+          }
+        )  
+  }
 }
